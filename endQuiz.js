6340f7dcc64a1c7f937e3623c4277716fa434e2f
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-01-06 16:21:24 -0600

    fixed href link to high scores

--- endQuiz.js
+++ endQuiz.js
@@ -25,5 +25,5 @@ saveHighScore = (event) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('highScores.html');
+    window.location.assign('https://brandyquinlan.github.io/HW04_Coding_Quiz/highScores.html');
 };
